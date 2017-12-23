6a0f77ce2337d3e554326958b9942376e56cda14
Sezer Ayran <sezer.ayran@outlook.com>
2017-12-23 16:02:26 +0200

    mvcBaslangic

---
+++ asp.NetMvc/mvcGiris/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcGiris.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // Views Klasörünün içindeki Home Klasöründe bulunan Index.cshtml i döner.
+            return View();
+        }
+    }
+}
---
+++ asp.NetMvc/mvcGiris/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcGiris.MvcApplication" Language="C#" %>
