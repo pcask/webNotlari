934384aee5663aa3d2f41897a020a5336626c4d2
Sezer Ayran <sezer.ayran@outlook.com>
2017-12-30 15:09:20 +0200

    1.BolumSonu

--- asp.NetMvc/mvcGiris/Controllers/HomeController.cs
+++ asp.NetMvc/mvcGiris/Controllers/HomeController.cs
@@ -9,9 +9,16 @@ namespace mvcGiris.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet] // Default olarak methodlarda Get işlemleri yapılır
         public ActionResult Index()
         {
             // Views Klasörünün içindeki Home Klasöründe bulunan Index.cshtml i döner.
+            return View();
+        }
+
+        [HttpPost] // Post işlemleri yapılacağı zaman Bu attribute method'a uygulanır.
+        public ActionResult PostIslemiYapilacak()
+        {
             return View();
         }
     }
