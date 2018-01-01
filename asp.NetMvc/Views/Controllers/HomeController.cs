549cc49b09c4a0daa4c7c07f649a6865b8c7cab6
Sezer Ayran <sezer.ayran@outlook.com>
2018-01-01 17:03:37 +0200

    Views Giriş ZenCoding Yüklenimi

---
+++ asp.NetMvc/Views/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Ana Sayfa
+        public ActionResult homePage()
+        {
+            return View();
+        }
+
+        // Hakkımızda
+        public ActionResult about()
+        {
+            return View();
+        }
+
+        // İletişim
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
