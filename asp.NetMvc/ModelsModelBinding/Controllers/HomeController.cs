80112bc2849ffbdc79d142184ba0f9ad66d30c3d
Sezer Ayran <sezer.ayran@outlook.com>
2018-01-07 12:30:20 +0200

    Model Kavrami Model Binding View lara özel birleşik modeller Bitiş

---
+++ asp.NetMvc/ModelsModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using ModelsModelBinding.Models;
+using ModelsModelBinding.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelsModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult homePage()
+        {
+
+            Kisi kisi = new Kisi()
+            {
+                Ad = "Sezer",
+                Soyad = "Ayran",
+                Yas=25
+            };
+
+            return View(kisi);
+        }
+
+        [HttpPost]
+        public ActionResult homePage(Kisi k)
+        {
+            return View(k);
+        }
+
+        public ActionResult viewlaraOzelModeller()
+        {
+            Kisi kisi = new Kisi
+            {
+                Ad = "Sezer",
+                Soyad = "Ayran",
+                Yas = 25
+            };
+
+            Adres adres = new Adres {
+                AdresTanimi = "Anonim Cad. Farazi Sok. No: 0",
+                Sehir = "İstanbul"
+            };
+
+            BirlesikModeller bm = new BirlesikModeller
+            {
+                AdresNesnesi = adres,
+                KisiNesnesi = kisi
+            };
+
+            return View(bm);
+        }
+
+        [HttpPost]
+        public ActionResult viewlaraOzelModeller(BirlesikModeller bm)
+        {
+            return View(bm);
+        }
+    }
+}
---
+++ asp.NetMvc/ModelsModelBinding/ViewModels/BirlesikModeller.cs
@@ -0,0 +1,14 @@
+﻿using ModelsModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelsModelBinding.ViewModels
+{
+    public class BirlesikModeller
+    {
+        public Kisi KisiNesnesi { get; set; }
+        public Adres AdresNesnesi { get; set; }
+    }
+}
