ee2f5232df64609884ddece0e63e871ba23de272
Sezer Ayran <sezer.ayran@outlook.com>
2018-01-10 21:05:28 +0200

    Bootstrap_HelperMethods Giriş

---
+++ asp.NetMvc/Bootstrap_HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap_HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult homePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ asp.NetMvc/Bootstrap_HelperMethods/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bootstrap_HelperMethods.MvcApplication" Language="C#" %>
